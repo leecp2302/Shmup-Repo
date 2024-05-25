a51f7ea46839e32809009f9b26e40004a8601ea4
leecp2302 <leecp2302@gmail.com>
2024-05-25 11:00:54 +1000

    Player 1

---
+++ Shmup/Assets/Scripts/Player.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Vector2 boundary;
+    private Camera MainCamera;
+    private float objectHeight;
+    private float objectWidth;
+    private Vector3 position;
+    public float speed = 400.0f;
+    public float tilt = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+
+        SetBoundary();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position = transform.position;
+
+        //PlayerMovement();
+
+        //Boundary();
+
+        transform.position = position;
+    }
+
+    void FixedUpdate()
+    {
+        PhysicsMovement();
+
+        Tilt();
+    }
+
+    void LateUpdate()
+    {
+        InitialiseOrthographicBoundary();
+
+        //InitialisePerspectiveBoundary();
+    }
+
+    private void PlayerMovement()
+    {
+        if (Input.GetKey("w"))
+        {
+            position.y += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            position.x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            position.y -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("d"))
+        {
+            position.x += speed * Time.deltaTime;
+        }
+    }
+
+    private void PhysicsMovement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f);
+        GetComponent<Rigidbody>().velocity = movement * speed * Time.fixedDeltaTime;
+    }
+
+    private void Tilt()
+    {
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * tilt * -1);
+    }
+
+    private void SetBoundary()
+    {
+        boundary = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        objectWidth = transform.GetComponent<MeshRenderer>().bounds.extents.x;
+        objectHeight = transform.GetComponent<MeshRenderer>().bounds.extents.y;
+    }
+
+    //private void Boundary()
+    //{
+    //    if (position.x > GameManager.instance.xBoundary)
+    //    {
+    //        position.x = GameManager.instance.xBoundary;
+    //    }
+    //    else if (position.x < -GameManager.instance.xBoundary)
+    //    {
+    //        position.x = -GameManager.instance.xBoundary;
+    //    }
+    //    if (position.y > GameManager.instance.yBoundary)
+    //    {
+    //        position.y = GameManager.instance.yBoundary;
+    //    }
+    //    else if (position.y < -GameManager.instance.yBoundary)
+    //    {
+    //        position.y = -GameManager.instance.yBoundary;
+    //    }
+    //}
+
+    private void InitialiseOrthographicBoundary()
+    {
+        Vector3 viewPosition = transform.position;
+        viewPosition.x = Mathf.Clamp(viewPosition.x, boundary.x * -1 + objectWidth, boundary.x - objectWidth);
+        viewPosition.y = Mathf.Clamp(viewPosition.y, boundary.y * -1 + objectHeight, boundary.y - objectHeight);
+        transform.position = viewPosition;
+    }
+
+    private void InitialisePerspectiveBoundary()
+    {
+        Vector3 viewPosition = transform.position;
+        viewPosition.x = Mathf.Clamp(viewPosition.x, boundary.x + objectWidth, boundary.x * -1 - objectWidth);
+        viewPosition.y = Mathf.Clamp(viewPosition.y, boundary.y + objectHeight, boundary.y * -1 - objectHeight);
+        transform.position = viewPosition;
+    }
+}
