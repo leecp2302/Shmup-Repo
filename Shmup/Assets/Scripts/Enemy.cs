1e0180b9726144d3b0e9aa46d44a93b8a59012c8
leecp2302 <leecp2302@gmail.com>
2024-05-31 12:42:36 +1000

    Damage 1

--- Shmup/Assets/Scripts/Enemy.cs
+++ Shmup/Assets/Scripts/Enemy.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private GameObject damage;
     public float health = 100.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        damage = GameObject.Find("Damage");
+        damage.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,9 +23,17 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(float damage)
     {
         health -= damage;
+        StartCoroutine(Damage());
         if(health == 0.0f)
         {
             Destroy(this.gameObject);
         }
+    }
+
+    private IEnumerator Damage()
+    {
+        damage.SetActive(true);
+        yield return new WaitForSeconds(0.05f);
+        damage.SetActive(false);
     }
 }
